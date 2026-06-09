916cb9fb045d9c2e7bde1e8718e4c2db9e913044
Jaideep <captamarsingh786@gmail.com>
2026-06-09 08:26:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener("load",()=>{
+
+    setTimeout(()=>{
+
+        document.getElementById("intro").style.display="none";
+
+        document.getElementById("main-site").style.display="block";
+
+    },13000);
+
+});
+const sideMenu = document.getElementById("sideMenu");
+
+const openMenu = document.getElementById("openMenu");
+
+const closeMenu = document.getElementById("closeMenu");
+
+openMenu.addEventListener("click", () => {
+
+    sideMenu.style.right = "0";
+
+});
+
+closeMenu.addEventListener("click", () => {
+
+    sideMenu.style.right = "-320px";
+
+});
