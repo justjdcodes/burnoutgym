69f31e11845fc55ca10a1456c8644358c9b2614d
Jaideep <captamarsingh786@gmail.com>
2026-06-13 00:20:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load",()=>{
 
         document.getElementById("main-site").style.display="block";
 
-    },8000);
+    },6000);
 
 });
 const sideMenu = document.getElementById("sideMenu");
